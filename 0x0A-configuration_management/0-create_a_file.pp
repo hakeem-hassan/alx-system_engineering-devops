06474097f5bd198a292d29282478424625bab9eb
hakeem-hassan <hlanre@ymail.com>
2024-07-04 02:42:49 -0400

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,12 @@
 #creat a file in /tmp using puppet
 
 file {'school':
-ensure  => 'file',
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love puppet',
+ ensure  => 'file',
+ path    => '/tmp/school',
+ mode    => '0744',
+ owner   => 'www-data',
+ group   => 'www-data',
+ content => 'I love puppet',
 }
 
 
