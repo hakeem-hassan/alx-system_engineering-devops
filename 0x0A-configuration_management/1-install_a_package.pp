55ba79646438b3578c4d39896361a45e25c333f5
hakeem-hassan <hlanre@ymail.com>
2024-07-04 03:08:58 -0400

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,19 @@
 #Install flask with puppet
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3.8':
+ ensure   => '3.8.10',
+ provider => 'pip3',
 }
+
+package { 'Flask':
+ ensure   => '2.1.0',
+ provider => 'pip3',
+}
+
+package { 'werkzeug':
+ ensure => '2.1.1',
+ provider => 'pip3',
+ require => Package['Flask'],
+} 
 
 
